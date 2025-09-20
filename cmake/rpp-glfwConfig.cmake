65851a7dd733b7a30c2a1099712e6b660c72bb78
threezinedine <threezinedine@gmail.com>
2025-09-20 22:33:05 +0700

    [configure] add glew for loading opengl apis

--- cmake/rpp-glfwConfig.cmake
+++ cmake/rpp-glfwConfig.cmake
@@ -2,10 +2,15 @@ if (rpp-glfw_FOUND)
     return()
 endif()
 
-set(rpp-glfw_DIR ${RPP_EXTERNALS_DIR}/glfw)
-add_subdirectory(${rpp-glfw_DIR} ${CMAKE_BINARY_DIR}/externals/glfw)
+set(glfw_DIR ${RPP_EXTERNALS_DIR}/glfw)
+add_subdirectory(${glfw_DIR} ${CMAKE_BINARY_DIR}/externals/glfw)
+
+set(glew_DIR ${RPP_EXTERNALS_DIR}/glew)
+add_subdirectory(${glew_DIR} ${CMAKE_BINARY_DIR}/externals/glew)
+
+find_package(OpenGL REQUIRED)
 
 add_library(rpp-glfw INTERFACE)
-target_link_libraries(rpp-glfw INTERFACE glfw)
+target_link_libraries(rpp-glfw INTERFACE glfw libglew_static OpenGL::GL)
 
 set(rpp-glfw_FOUND TRUE)
--- gruntime/main.cpp
+++ gruntime/main.cpp
@@ -1,3 +1,4 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <cstdio>
 #include "core/core.h"
@@ -21,9 +22,17 @@ int main(void)
     glfwMakeContextCurrent(window);
     rpp::PrintHello();
 
+    if (glewInit() != GLEW_OK)
+    {
+        fprintf(stderr, "Failed to initialize GLEW\n");
+        glfwTerminate();
+        return -1;
+    }
+
     while (!glfwWindowShouldClose(window))
     {
-        // glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
 
         // Render OpenGL here
 
