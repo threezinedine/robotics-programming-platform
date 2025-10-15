b482f89c658c71867ee7e67bcd9ac5ea6f0165de
Thao Nguyen The <threezinedine@gmail.com>
2025-10-15 18:15:57 +0700

    [feature] configure add vscode action

--- testui/main.py
+++ testui/main.py
@@ -1,13 +1,28 @@
+from warnings import filterwarnings
+
+filterwarnings("ignore", category=DeprecationWarning)
+
 from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6 import QtCore
 from converted_uis.main_window_ui import Ui_MainWindow
+
+
+class MyMainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+    def keyPressEvent(self, a0):
+        if a0.key() == QtCore.Qt.Key.Key_Escape:  # Escape key
+            self.close()
+        super().keyPressEvent(a0)
 
 
 class MyApplication(QApplication):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.main_window = QMainWindow()
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self.main_window)
+        self.main_window = MyMainWindow()
 
 
 def main():
